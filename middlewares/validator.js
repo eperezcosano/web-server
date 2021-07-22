b8423c070a2cdb2c630ad5b60a68fd5a25c9ea59
Izan Pérez Cosano <izanperez@yandex.com>
2021-07-22 15:48:06 +0200

    register

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -2,9 +2,8 @@ const { validationResult } = require('express-validator')
 
 function index(req, res, next) {
     // Validate form fields
-    //TODO: not working with activation and hcaptcha flags
     const errors = validationResult(req)
-    if (req.path !== '/register' && req.path !== '/login' && !errors.isEmpty()) {
+    if (req.path !== '/login' && !errors.isEmpty()) {
         const err = errors.array().reduce((obj, error) => Object.assign(obj, { [error.param]: error.msg }), {})
         return res.status(400).render('index', { errors: err })
     } else {
