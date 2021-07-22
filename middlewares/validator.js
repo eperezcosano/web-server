8ba6f67b16827113bf5db51b92120c676eae2ebd
Izan Pérez Cosano <izanperez@yandex.com>
2021-07-22 15:41:53 +0200

    and cond

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -4,7 +4,7 @@ function index(req, res, next) {
     // Validate form fields
     //TODO: not working with activation and hcaptcha flags
     const errors = validationResult(req)
-    if (req.path !== '/register' || req.path !== '/login' || !errors.isEmpty()) {
+    if (req.path !== '/register' && req.path !== '/login' && !errors.isEmpty()) {
         const err = errors.array().reduce((obj, error) => Object.assign(obj, { [error.param]: error.msg }), {})
         return res.status(400).render('index', { errors: err })
     } else {
