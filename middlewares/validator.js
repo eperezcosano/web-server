b2f6a45fbeea186e4b2d61d9350fc175d3dba86f
Izan Pérez Cosano <izanperez@yandex.com>
2021-07-22 15:51:44 +0200

    register page

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -5,7 +5,11 @@ function index(req, res, next) {
     const errors = validationResult(req)
     if (req.path !== '/login' && !errors.isEmpty()) {
         const err = errors.array().reduce((obj, error) => Object.assign(obj, { [error.param]: error.msg }), {})
-        return res.status(400).render('index', { errors: err })
+        if (req.path === '/register') {
+            return res.status(400).render('index', {register: {email: req.body.email}, errors: err})
+        } else {
+            return res.status(400).render('index', { errors: err })
+        }
     } else {
         next()
     }
