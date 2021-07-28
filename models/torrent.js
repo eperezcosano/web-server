4108ec06333a97444087e15395c4c3e623b42e4e
Izan Pérez Cosano <izanperez@yandex.com>
2021-07-28 22:04:23 +0200

    parse torrent

--- models/torrent.js
+++ models/torrent.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const torrentSchema = new mongoose.Schema({
         infoHash: {
             type: String,
-            required: true
+            required: true,
+            unique: true
         },
         name: {
             type: String,
