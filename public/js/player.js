a90a7a20e703872102dad0682fa43fa08741e8d7
Izan Pérez Cosano <izanperez@yandex.com>
2021-08-06 11:21:40 +0200

    player

--- public/js/player.js
+++ public/js/player.js
@@ -1,7 +1,10 @@
-const client = new WebTorrent()
+const client = new WebTorrent({
+    tracker: true,
+    dht: false,
+    lsd: false,
+    webSeeds: false
+})
 const infoHash = window.location.pathname.split('/')[2]
-console.log(infoHash)
-
 
 let body = document.body
 let progressBar = document.querySelector('#progressBar')
@@ -12,55 +15,56 @@ let remain = document.querySelector('#remaining')
 let uploadSpeed = document.querySelector('#uploadSpeed')
 let downloadSpeed = document.querySelector('#downloadSpeed')
 
-function download(torrentInput) {
-// Download the torrent
-client.add(torrentInput, function (torrent) {
+function download(infoHash) {
+    // Download the torrent
 
-    console.log(torrent.announce)
-    // Torrents can contain many files. Let's use the .mp4 file
-    let file = torrent.files.find(function (file) {
-        console.log(file)
-        return file.name.endsWith('.mp4')
-    })
+    client.add('/torrent/' + infoHash, function (torrent) {
 
-    // Stream the file in the browser
-    file.appendTo('output')
+        console.log(torrent.announce)
+        // Torrents can contain many files. Let's use the .mp4 file
+        let file = torrent.files.find(function (file) {
+            console.log(file)
+            return file.name.endsWith('.mp4')
+        })
 
-    // Trigger statistics refresh
-    torrent.on('done', onDone)
-    setInterval(onProgress, 500)
-    onProgress()
+        // Stream the file in the browser
+        file.appendTo('output')
 
-    // Statistics
-    function onProgress () {
-        // Peers
-        numPeers.innerHTML = torrent.numPeers + (torrent.numPeers === 1 ? ' peer' : ' peers')
-
-        // Progress
-        let percent = Math.round(torrent.progress * 100 * 100) / 100
-        progressBar.style.width = percent + '%'
-        downloaded.innerHTML = prettyBytes(torrent.downloaded)
-        total.innerHTML = prettyBytes(torrent.length)
-
-        // Remaining time
-        let remaining
-        if (torrent.done) {
-            remaining = 'Done.'
-        } else {
-            remaining = moment.duration(torrent.timeRemaining / 1000, 'seconds').humanize()
-            remaining = remaining[0].toUpperCase() + remaining.substring(1) + ' remaining.'
-        }
-        remain.innerHTML = remaining
-
-        // Speed rates
-        downloadSpeed.innerHTML = prettyBytes(torrent.downloadSpeed) + '/s'
-        uploadSpeed.innerHTML = prettyBytes(torrent.uploadSpeed) + '/s'
-    }
-    function onDone () {
-        body.className += ' is-seed'
+        // Trigger statistics refresh
+        torrent.on('done', onDone)
+        setInterval(onProgress, 500)
         onProgress()
-    }
-})
+
+        // Statistics
+        function onProgress () {
+            // Peers
+            numPeers.innerHTML = torrent.numPeers + (torrent.numPeers === 1 ? ' peer' : ' peers')
+
+            // Progress
+            let percent = Math.round(torrent.progress * 100 * 100) / 100
+            progressBar.style.width = percent + '%'
+            downloaded.innerHTML = prettyBytes(torrent.downloaded)
+            total.innerHTML = prettyBytes(torrent.length)
+
+            // Remaining time
+            let remaining
+            if (torrent.done) {
+                remaining = 'Done.'
+            } else {
+                remaining = moment.duration(torrent.timeRemaining / 1000, 'seconds').humanize()
+                remaining = remaining[0].toUpperCase() + remaining.substring(1) + ' remaining.'
+            }
+            remain.innerHTML = remaining
+
+            // Speed rates
+            downloadSpeed.innerHTML = prettyBytes(torrent.downloadSpeed) + '/s'
+            uploadSpeed.innerHTML = prettyBytes(torrent.uploadSpeed) + '/s'
+        }
+        function onDone () {
+            body.className += ' is-seed'
+            onProgress()
+        }
+    })
 }
 
 // Human readable bytes util
@@ -73,3 +77,5 @@ function prettyBytes(num) {
     unit = units[exponent]
     return (neg ? '-' : '') + num + ' ' + unit
 }
+
+download(infoHash)
